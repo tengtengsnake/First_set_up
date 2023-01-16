1a04c161e19a41e9feca5017f3be48186034dfc2
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2023-01-16 15:57:15 +0800

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 sudo apt update 
 sudo apt install apt-transport-https curl
-sudo apt install ibus-chewing
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 sudo apt update
+
+sudo apt install ibus-chewing
+
+sudo apt install libfuse # for balena etcher
 
 sudo apt install brave-browser
 
