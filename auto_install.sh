61ab3fe5296142b4342a45a5f1d66f5f55b41edc
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2023-03-18 12:31:13 +0800

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 sudo apt update 
 sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
 
 sudo apt install ibus-chewing
 sudo apt install vlock
